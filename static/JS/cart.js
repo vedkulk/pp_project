72fe570b397e69b8e3b74e980af1bbaef1397b63
Ved Kulkarni <kulkarnived3@gmail.com>
2024-03-27 13:35:51 +0530

    fixed js

--- static/JS/cart.js
+++ static/JS/cart.js
@@ -1,11 +1,11 @@
 let updateBtns = document.getElementsByClassName('update-cart')
 for (i=0;i<updateBtns.length;i++) {
     updateBtns[i].addEventListener('click', function(){
-        let productID = this.dataset.product;
-        let action= this.dataset.action;
+        let productID = this.dataset.product
+        let action= this.dataset.action
         console.log('productId: ', productID, 'action ', action)
-        console.log("User: ", user)
-        if(user==="ANONYMOUS")
+        console.log("USER: ", user)
+        if(user=="AnonymousUser")
         {
             console.log("Not logged in")
         }
@@ -17,19 +17,19 @@ for (i=0;i<updateBtns.length;i++) {
 
 function updateUserOrder(productID, action){
     console.log("User is logged in, sending data...");
-    var url = "/update_item"
-    fetch(url{
+    var url = "/update_item/"
+    fetch(url, {
         method:"POST",
         headers:{
-            'Content-type':'application/json'
+            'Content-type':'application/json',
+            'X-CSRFToken':csrftoken,
         },
         body:JSON.stringify({'productID': productID,'action':action})
-
-        .then((response)=>{
-            return response.json();
-        })
-        .then((data)=>{
-            console.log('data: ',data)
-        })
+    })
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log('data: ',data)
     })
 }
