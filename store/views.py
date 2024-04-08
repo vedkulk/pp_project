11fae9f7329eb2dbd34f9a956b683798e1103f89
Ved Kulkarni <kulkarnived3@gmail.com>
2024-04-08 20:40:17 +0530

    cart update

--- store/views.py
+++ store/views.py
@@ -80,7 +80,7 @@ def updateItem(request):
         orderItem.quantity = (orderItem.quantity+1)
     elif action == 'remove':
         orderItem.quantity = (orderItem.quantity-1)
-    order.save()
+    orderItem.save()
     if orderItem.quantity <= 0:
         orderItem.delete()
     return JsonResponse('Item was added.', safe=False)
