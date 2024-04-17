9410ff92420d47ebe1f7f7d87d406c0bd3acd3e3
Ved Kulkarni <kulkarnived3@gmail.com>
2024-04-17 21:15:13 +0530

    Login Authentication

--- store/models.py
+++ store/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    password = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
@@ -14,27 +14,27 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
-    price = models.DecimalField(max_digits=7,decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=False)
-    image = models.ImageField(null=True , blank=True)
+    image = models.ImageField(null=True, blank=True)
     author = models.CharField(max_length=100, null=True)
     number_of_pages = models.IntegerField(null=True)
     description = models.TextField(null=True)
+
     def __str__(self):
         return self.name
 
 
-
-
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
-
     def __str__(self):
         return str(self.id)
+
     @property
     def shipping(self):
         shipping = False
@@ -46,9 +46,10 @@ class Order(models.Model):
 
     @property
     def get_cart_total(self):
-        orderitems=self.orderitem_set.all()
-        total= sum([item.get_total for item in orderitems])
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
         return total
+
     @property
     def get_cart_items(self):
         orderitems = self.orderitem_set.all()
@@ -57,24 +58,29 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
     @property
     def get_total(self):
         total = self.product.price * self.quantity
         return total
 
+
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
     zipcode = models.CharField(max_length=200, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.address
--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import *
+from django.contrib import messages
 import json
 import datetime
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from .forms import CreateUserForm
 # Create your views here.
 
 
@@ -22,12 +26,46 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 
-def login(request):
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('store')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('store')
+            else:
+                messages.info(request, "The username or password is incorrect")
     return render(request, 'store/login.html')
 
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
 def Registration(request):
-    return render(request, 'store/Registration.html')
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        print("--------is valid", form.is_valid())
+        print(form.error_messages)
+        print(form.data["username"])
+        if form.is_valid():
+            user = form.save()
+            username = form.data["username"]
+            cust = Customer.objects.create(
+                user=user,
+                name=username
+            )
+            cust.save()
+            messages.success(request, "Successfully Registered")
+            return redirect('login')
+    context = {'form': form}
+    return render(request, 'store/Registration.html', context)
 
 
 def cart(request):
@@ -40,8 +78,8 @@ def cart(request):
         try:
             cart = json.loads(request.COOKIES['cart'])
         except:
-            cart={}
-        print('Cart :',cart)
+            cart = {}
+        print('Cart :', cart)
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     context = {'items': items, 'order': order}
@@ -58,32 +96,32 @@ def checkout(request):
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-        cartItems=order['get_cart-items']
+        cartItems = order['get_cart-items']
 
         for i in cart:
             try:
-                cartItems+=cart[i]["quantity"]
-                product=Product.objects.get(id=i)
-                total=(product.price*cart[i]['quantity'])
+                cartItems += cart[i]["quantity"]
+                product = Product.objects.get(id=i)
+                total = (product.price*cart[i]['quantity'])
 
-                order[get_cart_total]+=total
-                order[get_cart_items]+=cart[i]['quantity']
+                order["get_cart_total"] += total
+                order["get_cart_items"] += cart[i]['quantity']
 
                 item = {
 
-                    'product':{
-                        'id':product.id,
-                        'name':product.name,
-                        'price':product.price,
-                        'imgURL':product.imgURL,
-                        },
-                    'quantity':cart[i]['quantity'],
-                    'get_total':total
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imgURL': product.imgURL,
+                    },
+                    'quantity': cart[i]['quantity'],
+                    'get_total': total
                 }
                 items.append(item)
 
-                if product.digital==False:
-                    order['shipping']=True
+                if product.digital == False:
+                    order['shipping'] = True
             except:
                 pass
 
@@ -118,21 +156,24 @@ def updateItem(request):
     if orderItem.quantity <= 0:
         orderItem.delete()
     return JsonResponse('Item was added.', safe=False)
+
+
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
         if total == float(order.get_cart_total):
             order.complete = True
         order.save()
         if order.shipping == True:
-            ShippingAddress.objects.create( customer=customer, order=order,
-            address=data['shipping']['address'],city=data['shipping']['city'],
-            state=data['shipping']['state'],zipcode=data['shipping']['zipcode'])
+            ShippingAddress.objects.create(customer=customer, order=order,
+                                           address=data['shipping']['address'], city=data['shipping']['city'],
+                                           state=data['shipping']['state'], zipcode=data['shipping']['zipcode'])
     else:
         print("User is not logged in...")
     return JsonResponse('Payment completed', safe=False)
